a78296bbf22070c2199cd703ed81e902ae7e4cba
Warda Jawad <wardasobaih@gmail.com>
2023-01-06 19:40:36 +0200

    User's code Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'UserTypes/Delivery/delivery_ui.dart';
+import 'UserTypes/Admin/Screen/admin_main_tabs.dart';
 
 void main() {
   // runApp(MaterialApp(home: SplashScreen()));
+  // runApp(MaterialApp(home: UserInterfaces()));
   // runApp(MaterialApp(home: ServiceProviderUI()));
-  // runApp(MaterialApp(home: ServiceProviderUI()));
-  runApp(MaterialApp(home: DeliveryUI()));
-  // runApp(MaterialApp(home: AdminMainTabs()));
+  // runApp(MaterialApp(home: DeliveryUI()));
+  runApp(MaterialApp(home: AdminMainTabs()));
 }
