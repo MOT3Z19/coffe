bd6481bd0c927cb3e33226f52e935494685d0af4
Warda Jawad <wardasobaih@gmail.com>
2023-01-06 17:52:00 +0200

    Delivery's code Redfactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'UserTypes/Admin/Screen/admin_main_tabs.dart';
+import 'UserTypes/Delivery/delivery_ui.dart';
 
 void main() {
   // runApp(MaterialApp(home: SplashScreen()));
   // runApp(MaterialApp(home: ServiceProviderUI()));
-  // runApp(MaterialApp(home: AdminUI()));
-  runApp(MaterialApp(home: AdminMainTabs()));
+  // runApp(MaterialApp(home: ServiceProviderUI()));
+  runApp(MaterialApp(home: DeliveryUI()));
+  // runApp(MaterialApp(home: AdminMainTabs()));
 }
